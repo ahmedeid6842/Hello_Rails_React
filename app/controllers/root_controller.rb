e948ac06e032a40e49c025a175e6c9ad42cf7f68
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-11-07 23:15:02 +0200

    setup the root react

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,3 @@
 class RootController < ApplicationController
-  def index
-    random_greeting = Greeting.order("RANDOM()").first
-    render json: { greeting: random_greeting.message }
-  end
+  def index; end
 end
