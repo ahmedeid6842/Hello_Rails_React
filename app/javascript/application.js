7c70c236796b72610535360d01aecd8057044956
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-11-08 10:47:23 +0200

    Setup the react routes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,14 @@
+// Entry point for the build script in your package.json
+//import "@hotwired/turbo-rails"
+//import "./controllers"
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function App() {
-    return (<h1>Hello World!</h1>);
-}
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
 );
