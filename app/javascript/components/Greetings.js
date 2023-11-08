2f613b8b8266392b391a34b61197a96d1cc45925
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-11-08 10:47:12 +0200

    Created greetings component

---
+++ app/javascript/components/Greetings.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+
+const Greetings = () => {
+  return (
+    <div className="container">
+      <h1>Greeting component!</h1>
+    </div>
+  );
+};
+
+export default Greetings;
